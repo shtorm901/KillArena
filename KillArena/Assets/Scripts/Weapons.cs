5afa9c3dc063ef333d5f72aa17ca207ee56da07e
shtorm901 <59515836+shtorm901@users.noreply.github.com>
2024-05-30 20:33:48 +0300

    Были добавлены новые виды вооружений - Has been added new weapon type
    Были добавлены Топор и Молот
    Been added Axe and Hammer

--- KillArena/Assets/Scripts/Weapons.cs
+++ KillArena/Assets/Scripts/Weapons.cs
@@ -1,14 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu (fileName = "Weapons")]
 public class Weapons : ScriptableObject
 {
-    [Header("Данные")]
+    [Header("Основные данные")]
     public string Name;
     public string Description;
     public Sprite WeaponIcon;
+    public bool MinRangeWeapon;
+    public bool HighRangeWeapon;
 
-    [Header("Характеристики")]
+    [Header("Характеристики для всего оружия")]
     public int Damage;
     public int Endurance;
+
+    [Header("Характеристики для дальнего оружия")]
+    public int Range;
+    public int FireRate;
 }
